823e28b93040f51f438b1c3d0b5beac739235a08
pj <petejw@theswamp.co.uk>
2019-05-08 18:33:20 +0100

    Add libs

--- CustomerReport/Program.cs
+++ CustomerReport/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlexCel.Core;
+using FlexCel.Report;
 
 namespace CustomerReport
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var Customers = new List<Customer>();
+            Customers.Add(new Customer { Name = "Bill", Address = "555 demo line" });
+            Customers.Add(new Customer { Name = "Joe", Address = "556 demo line" });
+
+            using (FlexCelReport fr = new FlexCelReport(true))
+            {
+                fr.AddTable("Customer", Customers);
+                fr.Run("templates\\report.template.xlsx", "result.xlsx");
+            }
         }
+    }
+
+    class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
